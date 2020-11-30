d86329acd89c1e51c432ce35dbebfb79f045ac49
wangt15 <56089146+wangt15@users.noreply.github.com>
2020-11-30 18:43:11 -0500

    Create StudentTest.py

---
+++ StudentTest.py
@@ -0,0 +1,12 @@
+import unittest
+from Student import Student
+from Course import Course
+from Team import Team
+from Assignment import Assignment
+
+class StudentTest(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
